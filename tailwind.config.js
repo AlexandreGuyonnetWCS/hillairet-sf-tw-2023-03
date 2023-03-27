4b3a03dfe98dbe77b5ebb6383f73cbf76b8f9305
samSuffy <yobrambilla@gmail.com>
2023-03-27 10:33:08 +0200

    Initialize

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            'primary': 'rgba(214, 33, 40, 1)',
+            'secondary': 'rgba(252, 231, 28, 1)',
+            'primary-light': 'rgba(214, 33, 40, 0.5)',
+            'secondary-light': 'rgba(252, 231, 28, 0.5)',
+        },
+    },
+    plugins: [],
+}
